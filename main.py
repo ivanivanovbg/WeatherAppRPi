519ba434c176daa388dba3902508e0415a826f71
ivanivanovbg <i.ivanov.bg@abv.bg>
2021-10-12 15:05:28 +0300

    Weather app

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import json
 import tkinter as tk
 import tkinter.font
-import os
+from tkinter import messagebox
 from datetime import datetime
 from tkinter import *
 from tkinter import ttk
 
 import requests
-from PIL import ImageTk, Image
 
 
 # Date and time update function
@@ -30,95 +29,98 @@ def getweather():
     # Get data from API
     # API Key 9c0da7736675e795326c9c4898312686
     # API URL https://api.openweathermap.org/data/2.5/forecast?q=Burgas,bg&APPID=9c0da7736675e795326c9c4898312686&cnt=6&units=metric
-    apicount = 6
+    for j in range(len(datelabels)):
+        datelabels[j].destroy()
+        templabels[j].destroy()
+        fllabels[j].destroy()
+        preclabels[j].destroy()
+
+    datelabels.clear()
+    templabels.clear()
+    fllabels.clear()
+    preclabels.clear()
+
+    apicount = 8
     apiurl = "https://api.openweathermap.org/data/2.5/forecast?q=Burgas,bg&APPID=9c0da7736675e795326c9c4898312686&cnt=" + str(
         apicount) + "&units=metric"
 
     jsonresponse = requests.get(apiurl).text
     weatherinfo = json.loads(jsonresponse)
-    # array for labels
 
-    datelabels = []
-    templabels = []
-    fllabels = []
-    preclabels = []
-    iconlabels = []
+    col_start = 0
+    crow = 1
 
-    row_start = 1
     datestrl = ttk.Label(window)
     datestrl.config(text="Дата/час")
-    datestrl.grid(row=row_start, column=0, sticky=tk.E)
+    datestrl.grid(row=crow, column=col_start, sticky=tk.E)
+    datelabels.append(datestrl)
     tempstrl = ttk.Label(window)
     tempstrl.config(text="Температура")
-    tempstrl.grid(row=row_start + 1, column=0, sticky=tk.E)
+    tempstrl.grid(row=crow, column=col_start+1, sticky=tk.E)
+    templabels.append(tempstrl)
     flstrl = ttk.Label(window)
     flstrl.config(text="Усеща се")
-    flstrl.grid(row=row_start + 2, column=0, sticky=tk.E)
+    flstrl.grid(row=crow, column=col_start+2, sticky=tk.E)
+    fllabels.append(flstrl)
     popstrl = ttk.Label(window)
     popstrl.config(text="Валежи")
-    popstrl.grid(row=row_start + 3, column=0, sticky=tk.E)
+    popstrl.grid(row=crow, column=col_start+3, sticky=tk.E)
+    preclabels.append(popstrl)
 
+    crow += 1
     for i in range(apicount):
-        crow = row_start
         datestring = getmonthbul(datetime.fromtimestamp(weatherinfo["list"][i]["dt"]).strftime("%d-%b %H"))
         temperature = weatherinfo["list"][i]["main"]["temp"]
         feelslike = weatherinfo["list"][i]["main"]["feels_like"]
         precipitation = weatherinfo["list"][i]["pop"]
-        wxicon = weatherinfo["list"][i]["weather"][0]["icon"]
 
         datelabel = ttk.Label(window)
-        datelabel.grid(row=crow, column=i + 1, sticky=tk.N)
+        datelabel.grid(row=crow+i, column=col_start, sticky=tk.N)
         datelabel.config(text=datestring + ":00")
         datelabels.append(datelabel)
 
-        crow = crow + 1
         templabel = ttk.Label(window)
-        templabel.grid(row=crow, column=i + 1, sticky=tk.N)
+        templabel.grid(row=crow+i, column=col_start+1, sticky=tk.N)
         templabel.config(text=str(temperature) + " °C")
         templabels.append(templabel)
 
-        crow = crow + 1
         fllabel = ttk.Label(window)
-        fllabel.grid(row=crow, column=i + 1, sticky=tk.N)
+        fllabel.grid(row=crow+i, column=col_start+2, sticky=tk.N)
         fllabel.config(text=str(feelslike) + " °C")
         fllabels.append(fllabel)
 
-        crow = crow + 1
         preclabel = ttk.Label(window)
-        preclabel.grid(row=crow, column=i + 1, sticky=tk.N)
-        preclabel.config(text=str(precipitation * 100) + " %")
+        preclabel.grid(row=crow+i, column=col_start+3, sticky=tk.N)
+        preclabel.config(text=str(round(precipitation * 100)) + " %")
         preclabels.append(preclabel)
 
-        crow = crow + 1
-        scr_dir = os.path.dirname(__file__)
-        img_dir_rel = "images\\"
-        img_dir_abs = os.path.join(scr_dir,img_dir_rel)
-        dimage = Image.open(img_dir_abs + wxicon + "@2x.png")
-        dimage = dimage.resize((80, 80), Image.ANTIALIAS);
-        tkimage = ImageTk.PhotoImage(dimage)
-        iconlabel = ttk.Label(image=tkimage)
-        iconlabel.image = tkimage
-        iconlabel.grid(row=crow, column=i + 1, sticky=tk.N)
-        iconlabels.append(iconlabel)
+    window.after(120000,getweather)
 
+# arrays for the labels
+datelabels = []
+templabels = []
+fllabels = []
+preclabels = []
 
 window = tk.Tk()
 window.geometry("640x480")
 window.resizable(width=False, height=False)
+window.config(bg="#e6f542")
 window.title('Weather App')
+style = ttk.Style(window)
+style.configure("TLabel",background="#e6f542")
+style.configure("TLabel",padding="5")
 
 # Configure UI
 dfont = tkinter.font.nametofont("TkDefaultFont")
-dfont.config(size=9)
+dfont.config(size=12)
 dfont.config(family="Verdana")
 
-for row_num in range(window.grid_size()[1]):
-    window.rowconfigure(row_num, pad=5)
 
 # Label for date and time
 datetimelbl = ttk.Label(window)
 datetimelbl.grid(column=0, row=0, sticky=tk.N, padx=5, columnspan=9)
-datetimelbl.config(font=("Verdana", 14))
+datetimelbl.config(font=("Verdana", 18))
 update_clock()
 getweather()
 
